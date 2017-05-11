845b302e3eba5e0c680808469c7f9da840ee705f
Alejandro Rodriguez <alejandro.rodriguez@dasigno.com>
2017-05-11 10:10:14 -0500

    Initial Commit

---
+++ Momentum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Momentum
+//
+//  Created by Alejandro Rodriguez on 5/11/17.
+//  Copyright © 2017 ARF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
