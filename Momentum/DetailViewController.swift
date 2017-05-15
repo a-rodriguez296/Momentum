352865be1a307bf3eb1262016c8cc3df14a53a8d
Alejandro Rodriguez <alejandro.rodriguez@dasigno.com>
2017-05-15 11:21:01 -0500

    Detail VC

--- Momentum/DetailViewController.swift
+++ Momentum/DetailViewController.swift
@@ -9,19 +9,43 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var lblApplicationName: UILabel!
+    
+    @IBOutlet weak var cstToTopLayout: NSLayoutConstraint!
+    @IBOutlet weak var cstToThumbanil: NSLayoutConstraint!
+    
+    @IBOutlet weak var btnAppThumbnail: UIButton!
+    @IBOutlet weak var txtApplicationDescription: UITextView!
+    @IBOutlet weak var lblAppTitle: UILabel!
     
     var application:CDApplication?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        lblApplicationName.text = application!.name!
+        
+        btnAppThumbnail.setBackgroundImage(application?.photo?.image(), for: .normal)
+        lblAppTitle.text = application?.name!
+        txtApplicationDescription.text = application?.summary!
+        
     }
+
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         let _ = navigationController?.popToRootViewController(animated: true)
     }
+    
+    override func viewWillLayoutSubviews() {
+        txtApplicationDescription.setContentOffset(.zero, animated: true)
+    }
+    
+    
+    @IBAction func didPressThumbnail(_ sender: Any) {
+        
+        btnAppThumbnail.isHidden = true
+        
+        NSLayoutConstraint.deactivate([cstToThumbanil])
+        NSLayoutConstraint.activate([cstToTopLayout])
+        view.layoutIfNeeded()
+    }
+    
 }
